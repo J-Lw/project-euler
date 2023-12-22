6aa34653c6c3586f9a7aa4025d448987d5b543f0
J-Lw <JLaw0@proton.me>
2023-12-22 14:40:17 -0400

    p3 cp.

---
+++ p_euler/src/p3.rs
@@ -0,0 +1,16 @@
+/// [3](https://projecteuler.net/problem=3).
+///
+/// # p:
+/// - Find the largest prime factor of 600851475143.
+pub fn find_lpf() -> i32 { 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::;
+
+    #[test]
+    fn test() {
+        println!("{}", );
+    }
+}
